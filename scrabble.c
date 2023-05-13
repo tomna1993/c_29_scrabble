5f69ce7990b56b2e0149c3e4559aa2463739c5cb
TomasNagy <tomi.nagy@outlook.com>
2023-05-13 10:49:37 +0200

    Add comments and POINT array

---
+++ scrabble.c
@@ -0,0 +1,18 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+	// Points             A  B  C  D  E  F  G  H  I  J  K  L  M  N  O  P  Q   R  S  T  U  V  W  X  Y  Z
+	const int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+
+	// Get the word from the first player
+
+	// Get the word from the secong player
+
+	// Calculate the points of the first player
+
+	// Calculate the points of the second player
+
+	// Print out the winner
+}
